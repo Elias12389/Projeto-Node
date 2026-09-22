21f5dff390d63a2a2b4e7d9b8f8be42a354f0013
Elias12389 <eliasalveees@gmail.com>
2026-09-22 20:42:19 -0300

    Projetinho está fazendo o CRUD

--- modules/express.js
+++ modules/express.js
@@ -6,29 +6,81 @@ const UserModel = require("../src/models/user.model");
 //Inicializando o express
 const app = express();
 
+//Faz com que sempre usemos json nas nossas requisições (tipando sempre como Json)
+app.use(express.json());
+
 app.get("/home", (req, res) => {
   res.contentType("text/html");
   res.status(200).send("<h1>hello world</h1>");
 });
 
-app.get("/users", (req, res) => {
-  const users = [
-    {
-      name: "John Doe",
-      email: "john@doe.com",
-    },
-    {
-      name: "Jane Doe",
-      email: "jane@doe.com",
-    },
-  ];
+//Pega todos os usuários
+app.get("/users", async (req, res) => {
+  try {
+    //Método .find() serve como um buscador ou como um filtro para achar determinado objeto.
+    const users = await UserModel.find({});
+
+    res.status(200).json(users);
+  } catch (error) {
+    //Estamos somente passando uma string
+    return res.status(500).send(error.message);
+  }
   res.status(200).json(users);
 });
 
-app.post("/users", (req, res) => {
-  const user = UserModel.create(req.body);
+//End point para buscar um usuário por id
+app.get("/users/:id", async (req, res) => {
+  try {
+    //para dar certo, precisamos que o :id tenha o mesmo nome da variável
+    const id = req.params.id;
 
-  res.status(201).json(user);
+    const user = await UserModel.findById(id);
+
+    return res.status(200).json(user);
+  } catch (error) {
+    //Usamoso return para garantir que ele não execute nada após o send
+    return res.status(500).send(error.message);
+  }
+});
+
+//Criando um usuário
+//Usamos o post para criar alguma coisa.
+app.post("/users", async (req, res) => {
+  //Usamos o try e o catch para que ele tente executar. Se der erro, ele indica para o computador e não ficamos no escuro
+  try {
+    //o create trata-se de uma promisse, por isso podemos usar o async/await (precisamos na verdade)
+    const user = await UserModel.create(req.body);
+
+    res.status(201).json(user);
+  } catch (erro) {
+    res.status(500).send(error.message);
+  }
+});
+
+//Atualizar um usuário
+app.patch("/users/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    //Passamos os campos que queremos atualizar com o req.body
+    const user = await UserModel.findByIdAndUpdate(id, req.body, { new: true }); //Precisamos passar um objeto como verdadeiro (o new), pois o findByIdAndUpdate não retorna o registro atualizado.
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+//Deletando um usuário
+app.delete("/users/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const user = await UserModel.findByIdAndDelete(id);
+
+    return res.status(200).json(user);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 });
 
 const port = 8080;
